28a50188afe919b057d16d53320ce2866ba195f5
jiasonglin <jiasonglin001@ke.com>
2019-01-15 17:58:08 +0800

    新建路由逻辑

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import router from "./router";
+import router from "./router"
+import Axios from 'axios'
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
 Vue.config.productionTip = false
+Vue.prototype.$http = Axios
 
 new Vue({
   render: h => h(App),
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import VueRouter from "vue-router"
 import Introduce from "./pages/Introduce"
 import Posts from './pages/Posts'
 import Pages from './pages/Pages'
+import CreatPost from './pages/createPost';
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Introduce },
     { path: '/introduce', component: Introduce },
     { path: '/posts', component: Posts },
+    { path: '/creatPost', component: CreatPost },
     { path: '/pages', component: Pages },
   ]
 })
